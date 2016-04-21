7ed52c6b622f7143a4a57957bf10cd07351ca89f
zhangweican <515632400@qq.com>
2016-04-21 18:16:23 +0800

    移动cs-war-test项目的shiro，spring校验，I18N资源文件到cs-service

--- trunk/src/main/java/com/leweiyou/service/util/Commons.java
+++ trunk/src/main/java/com/leweiyou/service/util/Commons.java
@@ -2,4 +2,14 @@ package com.leweiyou.service.util;
 
 public class Commons {
 	public static final String ID_Ehcache_Default = "baseCache";
+	
+	public final static String SessionAcount = "com.leweiyou.session.key";
+	
+	/**
+	 * 定义校验失败存入Request的map的兼职
+	 */
+	public static final String Key_Valid_Error_Map = "Valid_Error_Map";
+	
+	public final static int MenuType_tree = 0;		//定义菜单类型为主菜单
+	public final static int MenuType_button = 1;	//定义菜单类型为按钮
 }
