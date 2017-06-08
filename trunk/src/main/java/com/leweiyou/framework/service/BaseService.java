a6cea17a9e0d4be2611be673a3f6bfe2825855ca
zhangweican <515632400@qq.com>
2017-06-08 14:44:06 +0800

    添加公共方法

--- trunk/src/main/java/com/leweiyou/framework/service/BaseService.java
+++ trunk/src/main/java/com/leweiyou/framework/service/BaseService.java
@@ -1,9 +1,13 @@
 package com.leweiyou.framework.service;
 
+import org.apache.log4j.Logger;
+
 /**
  * 基础服务类
  * @author Zhangweican
  *
  */
 public class BaseService {
+	/**日志操作对象**/
+	private Logger logger = Logger.getLogger(this.getClass());
 }
